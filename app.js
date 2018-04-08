10b46e30a0f8ea60768fc18d94d00951e8a2e4b1
aniket umbc <aniket.umbc@gmail.com>
2018-04-08 15:55:01 +0530

    change added DOMstrings for ui

--- app.js
+++ app.js
@@ -10,6 +10,29 @@ var budgetController = (function() {
 
 var  UIController =(function(){
 
+var DOMstrings = {
+    inputType:'.add__type',
+    inputDiscription:'.add__description',
+    inputValue:'.add__value',
+    inputAddBtn:'.add__btn',
+};
+
+
+
+    return{
+        getinput:function (){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, // either inc or expen
+                 discription: document.querySelector(DOMstrings.inputDiscription).value,
+                 value:document.querySelector(DOMstrings.inputValue).value
+            };          
+          },
+
+          getDOMstrings:function(){
+              return DOMstrings;
+          }
+    };
+
 })();
 
 
@@ -18,10 +41,18 @@ var  UIController =(function(){
 
 
 //app controller
+
 var controller =(function(budgetCtrl, UIctrl){
 
+    
+    var DOM = UIctrl.getDOMstrings();
+    
     var ctrlAddItem = function(){
         // get filed input data
+
+        var input = UIctrl.getinput();
+        console.log(input);
+
 
        //add the item to the budget controller
 
@@ -31,18 +62,15 @@ var controller =(function(budgetCtrl, UIctrl){
 
        //display budget
 
-       alert("Hello Aniket");
-
     }
 
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputAddBtn).addEventListener('click',ctrlAddItem);
 
     document.addEventListener('keypress',function(event){
        if(event.keyCode === 13 || event.which === 13){
         ctrlAddItem();
        }           
     });
-
 
 })(budgetController,UIController); 
 
