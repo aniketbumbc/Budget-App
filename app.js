83c5ba009e12593ef188e550014822e06116dd55
aniket umbc <aniket.umbc@gmail.com>
2018-04-08 16:13:31 +0530

    added function constructor expense and income

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var budgetController = (function () {
 
+    //function constructor 
+    
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
 })();
 
 
