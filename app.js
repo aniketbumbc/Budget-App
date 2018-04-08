344fc18ddaca696a64cb88a97597ba4dcba9b192
aniket umbc <aniket.umbc@gmail.com>
2018-04-08 15:20:47 +0530

    added keyboard event

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
+
+
 var budgetController = (function() {
 
 })();
 
+
+
+
+
 var  UIController =(function(){
-
-
 
 })();
 
 
 
+
+
+
+//app controller
 var controller =(function(budgetCtrl, UIctrl){
+
+    document.querySelector('.add__btn').addEventListener('click',function(){
+       // get filed input data
+
+       //add the item to the budget controller
+
+       //add the item to the UI
+
+       //calculate budget
+
+       //display budget
+    });
+
+    document.addEventListener('keypress',function(event){
+       if(event.keyCode === 13 || event.which === 13){
+        console.log("Enter Press");
+       }
+           
+    });
 
 
 })(budgetController,UIController); 
+
+
+
+
+
+
+
+
+
